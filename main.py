3ae06a19cea8a51aa093b352067307f6c5ef156a
Agata Konarska <konarskaagata2001@gmail.com>
2024-03-24 11:05:42 +0100

    New shapes added

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import math
+
 bok_a = 10.4
 
 pole_kw = (bok_a * bok_a)
 
 print(pole_kw)
+
+bok_a = 3
+bok_b = 7
+bok_c = 5
+wys_c = 2
+
+pole_kw = (bok_a*bok_a)
+pole_tr = (bok_b*bok_b*math.sqrt(3)/4)
+pole_pr = (bok_a*bok_c)
+pole_tr = (bok_c*wys_c)
+
+print(pole_kw, pole_tr, pole_pr, pole_tr)
