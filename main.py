dfc6b84bc58123d199d4767a8cc63ab4726c2a95
Agata Konarska <konarskaagata2001@gmail.com>
2024-03-24 11:19:45 +0100

    Some blocks have been added

--- main.py
+++ main.py
@@ -17,3 +17,19 @@ pole_pr = (bok_a*bok_c)
 pole_tr = (bok_c*wys_c)
 
 print(pole_kw, pole_tr, pole_pr, pole_tr)
+
+bok_x = 6
+bok_y = 8
+bok_z = 9
+
+wys_alfa = 7.89
+promien_alfa = 3.34
+
+wys_beta = 9.6
+promien_beta = 2.7
+
+obj_prostopadloscian = (bok_x * bok_y * bok_z)
+obj_walec = (math.pi * promien_alfa * promien_alfa * wys_alfa)
+obj_stozek = (math.pi * promien_beta * promien_beta * wys_beta /3)
+
+print (obj_prostopadloscian, obj_walec, obj_stozek)
